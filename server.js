9877d78886ab296f4c4448918060f719ff3e93f7
Divine Nsun <nsundivine6@gmail.com>
2026-09-21 22:38:24 +0100

    Prepare app for public hosting

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
